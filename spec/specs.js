8a88c5798e0ffee3cb0cd199acfccd077f549a4a
Alexa Zeazas & Jennifer Bell <student@epicodus.com>
2015-09-09 09:55:33 -0700

    Add spec for adding consonant + 'ay' at the end of the word.

--- spec/specs.js
+++ spec/specs.js
@@ -2,4 +2,8 @@ describe('pigLatin', function() {
   it("add ay to the end of a word", function() {
     expect(pigLatin("obiwan")).to.equal("obiwanay");
   });
+
+  it("adds consonant with ay to the end of the word", function() {
+    expect(pigLatin("vader")).to.equal("adervay");
+  });
 });

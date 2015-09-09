0b24760d40a8aecbcc45210909e5525bcbb5f99b
Alexa Zeazas & Jennifer Bell <student@epicodus.com>
2015-09-09 10:47:00 -0700

    Add code for adding consonant + 'ay' at the end of the word, pass.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,15 @@
 var pigLatin = function(text) {
-  return text + "ay";
+  var array = text.split(" ");
+  var newArray = []
+  array.forEach(function(word) {
+    if (word[0].match(/[aeiou]/)) {
+      word += "ay";
+    } else {
+      var pre = word.match(/\b([bcdfghjklmnprstvwxyz]|qu)+/)[0];
+      var post = word.slice(pre.length);
+      word = post + pre + "ay";
+    }
+    newArray.push(word);
+  });
+  return newArray.join();
 };
